d6be2c6f3608b5062147fbe59c445401f5f2e975
thongho98 <hoquocthong456@gmail.com>
2019-07-09 19:51:33 +0700

    Commit WebConfig

--- src/main/java/com/shopper/web/saleshoes/controller/ProductsController.java
+++ src/main/java/com/shopper/web/saleshoes/controller/ProductsController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
